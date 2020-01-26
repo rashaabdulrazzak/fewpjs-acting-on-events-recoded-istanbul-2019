e2b89bb423165516ab8eaf21b97734901fd9d290
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-01-26 19:18:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ function moveDodgerRight() {
     dodger.style.left = `${left + 1}px`;
   }
 }
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  console.log(leftNumbers);
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowRight") {
     moveDodgerRight();
