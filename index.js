0733b2cd7934901b307f1c7d277a23fa0b3f889a
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-01-26 19:05:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 // Your code here
 let dodger = document.getElementById("dodger");
-dodger.style.backgroundColor = "#000000";
-dodger.style.backgroundColor = "#FF69B4";
-dodger.style.left; 
-dodger.style.bottom;
-dodger.style.bottom = "100px";
-dodger.style.bottom = "0px";
-/*document.addEventListener("keydown", function(e) {
-  console.log(e.key);
-});*/
-document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
-    let leftNumbers = dodger.style.left.replace("px", "");
-    let left = parseInt(leftNumbers, 10);
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
 
-    dodger.style.right = `${left - 25}px`;
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
   }
-});
+}
